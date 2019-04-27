d02b8a20c39690333c119ff95e966c4949e9f9c7
lechestercat <chesterortega1991@gmail.com>
2019-04-27 02:00:27 -0400

    final commit

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 var butt = document.querySelector("button");
+var isBlue = false;
+
+butt.addEventListener("click",function(){
+    if(isBlue){
+        document.body.style.backgroundColor = "white";
+        isBlue = false;
+    }else{
+        document.body.style.backgroundColor = "blue";
+        isBlue = true;
+    }
+}
+);
